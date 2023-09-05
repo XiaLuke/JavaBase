8df71f70491b03d6ac40ddab35973ab6e552cd85
Travike <xiafan98@163.com>
2023-09-05 15:53:58 +0800

    Chapter IV Object init

---
+++ java_core_technology/src/main/java/CHAPTER_IV_ObjectAndClass/_2_Class/ClassIntro.java
@@ -0,0 +1,27 @@
+package CHAPTER_IV_ObjectAndClass._2_Class;
+
+public class ClassIntro {
+    /**
+     * 4.1.1
+     * Class
+     */
+    public void class_() {
+        // class is a template for creating objects
+        // The process of constructing an object from a class is called creating an instance of the class
+
+        // encapsulation
+        // Is an important concept for dealing with objects,
+        // encapsulation is the combination of data and behavior in a package,
+        // and hide the specific implementation from the user of the object.
+
+        // The data in the object becomes an instance field,
+        // and the process of manipulating the data becomes a method.
+
+        // The key to implementing encapsulation is that methods in a class must never directly access instance fields of other classes.
+        // Programs can only interact with object data through object methods。
+        // Encapsulation gives objects a "black box" property: it doesn't matter what's in the box, just how to interact with the box.
+        // improved reusability and operability
+
+        //
+    }
+}
